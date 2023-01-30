e7d0ece11bc46e2424a8d413cb9fb3deb85e75e2
Vinicius Gomes <vinicius.falcao@capgemini>
2023-01-30 12:01:26 -0300

    Remove dist folder

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const crawler_1 = require("./crawler");
-const crawler = new crawler_1.Crawler('resources/sitemap/sitemap.xml');
-crawler.navigateAndTakePrint();
-crawler.createCSVFile();
-//# sourceMappingURL=main.js.map
